229ebe8d944b191ef19147f0e16773f1a3b44a09
chungchamchi <chung.dinh@employmenthero.com>
2024-08-11 18:22:09 +0700

    feat: add command FF to format file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
--- test save file
+
+local nvim_create_cmd = vim.api.nvim_create_user_command
+
+nvim_create_cmd("FF", function()
+  LazyVim.format({ force = true })
+end, { desc = "Format" })
